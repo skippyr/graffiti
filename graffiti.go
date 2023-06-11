9097b0726eb7a24979ca51b1997866d0dcb997ac
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-11 11:45:59 -0300

    refactor: adjust naming convention

--- graffiti.go
+++ graffiti.go
@@ -4,46 +4,48 @@ import (
 	"fmt"
 )
 
-func create_style_sequence(code int) string {
+func createStyleSequence(code int) string {
 	return fmt.Sprintf("\x1b[%dm", code)
 }
 
 func style(text string) string {
-	simple_format_specifiers := map[rune]int {
+	simpleFormatSpecifiers := map[rune]int {
 		'B': 1, // Bold
 		'I': 3, // Italic
 		'U': 4, // Underline
+		'r': 0, // Reset
 	}
-	is_formatting := false
-	has_style := false
-	styled_text := ""
+	isFormatting := false
+	hasStyle := false
+	styledText := ""
 	for
 		characters_iterator := 0;
 		characters_iterator < len(text);
 		characters_iterator ++ {
 		character := rune(text[characters_iterator])
 		if character == '%' {
-			if is_formatting {
-				is_formatting = false
+			if isFormatting {
+				isFormatting = false
 			} else {
-				is_formatting = true
+				isFormatting = true
 			}
 		}
-		if !is_formatting {
-			styled_text = styled_text + string(character)
+		if !isFormatting {
+			styledText = styledText + string(character)
+			continue
 		}
-		for format_specifier, format_code := range simple_format_specifiers {
-			if character == format_specifier {
-				styled_text = styled_text + create_style_sequence(format_code)
-				is_formatting = false
-				has_style = true
+		for formatSpecifier, formatCode := range simpleFormatSpecifiers {
+			if character == formatSpecifier {
+				styledText = styledText + createStyleSequence(formatCode)
+				isFormatting = false
+				hasStyle = true
 			}
 		}
 	}
-	if has_style {
-		styled_text = styled_text + create_style_sequence(0)
+	if hasStyle {
+		styledText = styledText + createStyleSequence(0)
 	}
-	return styled_text
+	return styledText
 }
 
 func StdoutWrite(text string) {
