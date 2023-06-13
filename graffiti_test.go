0b5b5ce0bdeade844907f89f4d2e9a348d034938
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-13 11:00:27 -0300

    refactor: test ansi escape sequence delimiters separately

--- graffiti_test.go
+++ graffiti_test.go
@@ -6,20 +6,20 @@ import (
 )
 
 func TestRemoveAnsiEscapeSequences(test *testing.T) {
-	sample := ""
-	expectedResult := ""
 	for delimitersIterator, delimiter := range ansiEscapeSequencesDelimiters {
-		sample = sample + " \x1b[30" + string(delimiter) + strconv.Itoa(delimitersIterator)
-		expectedResult = expectedResult + " " + strconv.Itoa(delimitersIterator)
-	}
-	result := removeAnsiEscapeSequences(&sample)
-	if result != expectedResult {
-		test.Errorf(
-			"Failed to remove ANSI escape sequences. Expected \"%s\" (%d characters) but received \"%s\" (%d characters).",
-			expectedResult,
-			len(expectedResult),
-			result,
-			len(result),
-		)
+		sample := "\x1b[30" + string(delimiter) + strconv.Itoa(delimitersIterator)
+		expectedResult := strconv.Itoa(delimitersIterator)
+		result := removeAnsiEscapeSequences(&sample)
+		if result != expectedResult {
+			test.Errorf(
+				"Failed to remove ANSI escape sequence with delimiter \"%c\". Expected \"%s\" (%d characters) but received \"%s\" (%d characters).",
+				delimiter,
+				expectedResult,
+				len(expectedResult),
+				result,
+				len(result),
+			)
+		}
 	}
 }
+
